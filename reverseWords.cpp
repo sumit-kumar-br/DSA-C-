3fe59571bd90b0a85c34094b59f3ad05cc4d141f
Sumit Kumar <roys930430@gmail.com>
2024-11-04 22:31:57 +0530

    Reversing every word in a sentence

--- reverseWords.cpp
+++ reverseWords.cpp
@@ -6,14 +6,17 @@ output - yM eman si timuS
 #include <iostream>
 using namespace std;
 
-string reverse(string st) {
+string reverse(string st)
+{
   string rev = "";
-  for(int i=st.length()-1; i>=0; i--) {
+  for (int i = st.length() - 1; i >= 0; i--)
+  {
     rev += st[i];
   }
   return rev;
 }
 
+/*
 string getReverse(string st) {
   string rev = "";
   for(int i=0; i<st.length()-1; i++) {
@@ -24,7 +27,30 @@ string getReverse(string st) {
   }
   return rev;
 }
-int main() {
+*/
+
+string getReverse(string st)
+{
+  string rev = "";
+  int start = 0;
+
+  for (int i = 0; i <= st.length(); i++)
+  {
+    if (i == st.length() || st[i] == ' ')
+    {
+      rev += reverse(st.substr(start, i - start));
+      if (i < st.length())
+      {
+        rev += " ";
+      }
+      start = i + 1;
+    }
+  }
+  return rev;
+}
+
+int main()
+{
   string name;
   cout << " Enter the string: " << endl;
   getline(cin, name);
