76f3847d0b52428243fb4f793f1320e8b3c2c76b
Sumit Kumar <roys930430@gmail.com>
2025-03-09 10:29:19 +0530

    implementation of sieve of eratosthenes

--- sieveOfEratosthenes.cpp
+++ sieveOfEratosthenes.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 int countPrimes(int n) {
-  int cnt = 0;
+  int count = 0;
   vector<bool> prime(n+1, true);
   prime[0] = prime[1] =false;
   for(int i=2; i<n; i++) {
     
     if(prime[i]) {
-      cnt++;
+      count++;
       for(int j=i*2; j<n; j+=i) {
         prime[j] = false;
       }
     }
   }
-  return cnt;
+  return count;
 }
 
 int main() {
